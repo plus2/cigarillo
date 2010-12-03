a9893ee055568e8a2d03e03e667b4c0399189962
Lachie Cox <lachiec@gmail.com>
2010-12-03 16:10:21 +1100

    be more picky about db keys

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -6,7 +6,12 @@ act :on => 'yesmaster/prepare_app' do
 
   db = node.databases.find {|d| d.env == node.rack_env}
 
-  node.cigarillo.coordinator.mongo = db
+  node.cigarillo.coordinator.mongo = {
+    'host' => db.host,
+    'username' => db.username,
+    'password' => db.password,
+    'database' => db.id
+  }
 
   file path+'config.yml', :string => node.cigarillo.worker.to_yaml
   file path+'coord.yml' , :string => node.cigarillo.coordinator.to_yaml
