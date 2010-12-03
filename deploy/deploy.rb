46d7114e34d490af0a51f663cae8168527ff06ec
Lachie Cox <lachiec@gmail.com>
2010-12-03 17:17:10 +1100

    protect against db not being there (as it isn't in the worker)

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -4,14 +4,14 @@ require 'pathname'
 act :on => 'yesmaster/prepare_app' do
   path = Pathname(node.current_path)
 
-  db = node.databases.find {|d| d.env == node.rack_env}
-
-  node.cigarillo.coordinator.mongo = {
-    'host' => db.host,
-    'username' => db.username,
-    'password' => db.password,
-    'database' => db.id
-  }
+  if db = node.databases.find {|d| d.env == node.rack_env}
+    node.cigarillo.coordinator.mongo = {
+      'host' => db.host,
+      'username' => db.username,
+      'password' => db.password,
+      'database' => db.id
+    }
+  end
 
   file path+'config.yml', :string => node.cigarillo.worker.to_yaml
   file path+'coord.yml' , :string => node.cigarillo.coordinator.to_yaml
