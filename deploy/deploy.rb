38258b5d9973c90124b7f261413058d5dd85d3ec
Lachie Cox <lachiec@gmail.com>
2010-12-03 15:57:49 +1100

    finished off deploy act

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -2,5 +2,12 @@ require 'yaml'
 require 'pathname'
 
 act :on => 'yesmaster/prepare_app' do
-  file Pathname(node.current_path)+'config.yml', :string => node.cigarillo.to_yaml
+  path = Pathname(node.current_path)
+
+  db = node.databases.find {|d| d.env == node.rack_env}
+
+  node.cigarillo.coordinator.mongo = db
+
+  file path+'config.yml', :string => node.cigarillo.worker.to_yaml
+  file path+'coord.yml' , :string => node.cigarillo.coordinator.to_yaml
 end
