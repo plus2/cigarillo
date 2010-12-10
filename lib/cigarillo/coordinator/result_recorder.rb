7f81987e996a02fcc570300aeb2ce99273f01cea
Lachie Cox <lachiec@gmail.com>
2010-12-10 11:38:15 +1100

    expand the message sent after a build is recorded

--- lib/cigarillo/coordinator/result_recorder.rb
+++ lib/cigarillo/coordinator/result_recorder.rb
@@ -9,11 +9,13 @@ module Cigarillo
         payload = env['igor.payload']
         if payload['cigarillo-kind'] == 'result'
           puts "recording result"
-          build_id = payload.delete('build_id')
-          Build.record_result(build_id,payload.tapp(:payload)) if build_id
+          if build_id = payload.delete('build_id')
+            Build.record_result(build_id,payload)
 
-          # XXX expand message!
-          {:exchange => 'plus2.messages', :app => 'cigarillo-coord', :msg => 'build done'}
+            build = Build.find(build_id)
+
+            {:exchange => 'plus2.messages', :app => 'cigarillo-coord', :msg => "[#{build.repo.path_name}] build #{build.succeeded? ? "succeeded" : "failed"} http://ci.plus2dev.com/builds/#{build_id}"}
+          end
         else
           @igor.call(env)
         end
