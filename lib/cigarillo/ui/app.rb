1b1ece3a4b8d1072c3e9623c241c68b3b4c99e4d
Lachie Cox <lachiec@gmail.com>
2010-12-17 11:27:02 +1100

    various helpers and refactoring to support pretty look

--- lib/cigarillo/ui/app.rb
+++ lib/cigarillo/ui/app.rb
@@ -7,6 +7,26 @@ module Cigarillo
       
       enable :logging, :dump_errors
 
+      before do
+        @repos_menu = Cigarillo::Coordinator::Repo.all_by_last_seen_at(5).to_a
+        @more_menu_repos = Cigarillo::Coordinator::Repo.count > @repos_menu.size
+
+        @submenu_active = {}
+      end
+      helpers do
+        def active(key)
+          if @submenu_active && @submenu_active == key
+            {:class => 'active'}
+          else
+            {}
+          end
+        end
+      end
+
+      def remove_current_repo_from_menu
+        @repos_menu.delete_if {|repo| repo._id == @repo._id}
+      end
+
       get '/' do
         @css = %W{index}
         @repos = Cigarillo::Coordinator::Repo.all
@@ -16,32 +36,50 @@ module Cigarillo
       get '/repos/:id' do |id|
         @css = %W{repo}
         @repo = Cigarillo::Coordinator::Repo.find(id)
-        haml :repo
+
+        remove_current_repo_from_menu
+
+        if params[:setup]
+          @submenu_active = 'setup'
+          haml :repo_setup
+        else
+          @submenu_active = 'builds'
+          haml :repo
+        end
       end
 
+      get '/builds/:id' do |id|
+        @css = %W{build}
+        @build = Cigarillo::Coordinator::Build.find(id)
+
+        @repo  = @build.repo
+        remove_current_repo_from_menu
+        @submenu_active = 'builds'
+
+        haml :build
+      end
+
+      # no view
       get '/repos/:id/ci/:ref' do |id,ref|
         @repo = Cigarillo::Coordinator::Repo.find(id)
         @repo.add_ref_to_ci(ref)
         redirect "/repos/#{@repo._id}"
       end
 
+      # no view
       get '/repos/:id/no-ci/:ref' do |id,ref|
         @repo = Cigarillo::Coordinator::Repo.find(id)
         @repo.del_ref_from_ci(ref)
         redirect "/repos/#{@repo._id}"
       end
 
+      #no view
       get '/repos/:id/force-build/:ref' do |id,ref|
         @repo = Cigarillo::Coordinator::Repo.find(id)
         @build = @repo.force_build!(ref)
         redirect "/builds/#{@build._id}"
       end
 
-      get '/builds/:id' do |id|
-        @css = %W{build}
-        @build = Cigarillo::Coordinator::Build.find(id)
-        haml :build
-      end
     end
   end
 end
