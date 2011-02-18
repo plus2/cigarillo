1e1f2d8b5a6be59840432d92fd37357711e70556
Lachie Cox <lachiec@gmail.com>
2011-02-18 17:04:30 +1100

    split into various run-styles

--- lib/cigarillo/integration/runner.rb
+++ lib/cigarillo/integration/runner.rb
@@ -5,36 +5,20 @@ module Cigarillo
   module Integration
     class Runner
       def call(env)
-        progress = Cigarillo::Utils::LineProgress.new(env['progress'])
 
         builds = env['ci']['environments'].map do |environment,options|
           unless options['build_command']
             next :env => environment, :result => nil
           end
 
-          cmd = {
-            :cmd => "script -q ci.output -c 'bundle exec #{options['build_command']}'",
-            :cwd => env['runner.cwd'],
-            :stream => true,
-            :environment => {'RAILS_ENV' => environment, 'RACK_ENV' => environment}
-          }
-
-          result = AngryShell::Shell.new.popen4(cmd) do |cid,ipc|
-            readers = [ipc.stdout, ipc.stderr]
-
-            until readers.empty?
-              ready = IO.select(readers, nil, nil, 1.0) || next
-
-              begin
-                ready[0].each {|fd| 
-                  tag = fd == ipc.stdout ? :out : :err
-                  progress.add tag, fd.read_nonblock(512) #.taputs
-                }
-              rescue EOFError
-                readers.delete_if {|fd| fd.eof?}
-              end
-            end
-          end
+          result = case options['progress']
+                   when 'structured'
+                     structured(env, environment, options)
+                   when 'piped'
+                     piped(env, environment, options)
+                   when 'tty',nil
+                     tty(env, environment, options)
+                   end
 
           {:env => environment, :success => result.ok?}
         end
@@ -43,6 +27,73 @@ module Cigarillo
         Cigarillo::Utils::Result.new(env).finish! do |res|
           res.details[:builds] = builds
           res.status builds.all? {|res| res[:success]} ? 'ok' : 'fail'
+        end
+      end
+
+      protected
+
+      def tty(env, environment, options)
+        progress = Cigarillo::Utils::LineProgress.new(env['progress'])
+
+        # XXX script -c is linux only
+        cmd = {
+          :cmd => "script -q ci.output -c 'bundle exec #{options['build_command']}'",
+          :cwd => env['runner.cwd'],
+          :stream => true,
+          :environment => {'RAILS_ENV' => environment, 'RACK_ENV' => environment}
+        }
+
+        run_with_progress(cmd, progress)
+      end
+
+
+      def piped(env, environment, options)
+        progress = Cigarillo::Utils::LineProgress.new(env['progress'])
+
+        cmd = {
+          :cmd => "bundle exec #{options['build_command']}",
+          :cwd => env['runner.cwd'],
+          :stream => true,
+          :environment => {'RAILS_ENV' => environment, 'RACK_ENV' => environment}
+        }
+
+        run_with_progress(cmd, progress)
+      end
+
+
+      def structured(env, environment, options)
+        progress = Cigarillo::Utils::StructuredProgress.new(env['progress'])
+
+        rubyopt = "-I#{Cigarillo.root+'lib'} -rcigarillo"
+
+        cmd = {
+          :cmd => "bundle exec #{options['build_command']}",
+          :cwd => env['runner.cwd'],
+          :stream => true,
+          :environment => {'RAILS_ENV' => environment, 'RACK_ENV' => environment, 'RUBYOPT' => rubyopt, 'BUNDLE_GEMFILE' => nil, 'GEM_HOME' => nil, 'GEM_PATH' => nil},
+          :without_cleaning_bundler => true
+        }
+
+        run_with_progress(cmd, progress)
+      end
+
+
+      def run_with_progress(cmd, progress)
+        AngryShell::Shell.new.popen4(cmd) do |cid,ipc|
+          readers = [ipc.stdout, ipc.stderr]
+
+          until readers.empty?
+            ready = IO.select(readers, nil, nil, 1.0) || next
+
+            begin
+              ready[0].each {|fd| 
+                tag = fd == ipc.stdout ? :out : :err
+                progress.add tag, fd.read_nonblock(512) #.taputs
+              }
+            rescue EOFError
+              readers.delete_if {|fd| fd.eof?}
+            end
+          end
         end
       end
 
