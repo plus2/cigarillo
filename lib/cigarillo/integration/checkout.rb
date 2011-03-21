2c5f7c766777725a65171e81f0b60af9bd598553
Lachie Cox <lachiec@gmail.com>
2011-03-21 17:05:15 +1100

    more tweex

--- lib/cigarillo/integration/checkout.rb
+++ lib/cigarillo/integration/checkout.rb
@@ -31,7 +31,8 @@ module Cigarillo
             progress.task("git.submodules") { repo.submodules! }
 
             progress.task("bundle") {
-              sh("bundle install --local --without no_ci --path=#{Cigarillo.workbench+'bundle'}", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
+              # sh("bundle install --local --without no_ci --path=#{Cigarillo.workbench+'bundle'}", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
+              sh("bundle install --deployment --without no_ci", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
             }
           end
 
