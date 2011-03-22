fe04dee9f22a74d00273d7bea6966c8a024148c4
Lachie Cox <lachiec@gmail.com>
2011-03-23 10:18:23 +1100

    path, but not local

--- lib/cigarillo/integration/checkout.rb
+++ lib/cigarillo/integration/checkout.rb
@@ -31,9 +31,7 @@ module Cigarillo
             progress.task("git.submodules") { repo.submodules! }
 
             progress.task("bundle") {
-              # sh("bundle install --local --without no_ci --path=#{Cigarillo.workbench+'bundle'}", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
-              # sh("bundle install --deployment --without no_ci", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
-              sh("bundle install --without no_ci", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
+              sh("bundle install --path=#{Cigarillo.workbench+'bundle'} --without no_ci", :cwd => repo.checkout).run if (repo.checkout+'Gemfile').exist?
             }
           end
 
